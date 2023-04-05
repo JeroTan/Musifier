dc5a6eaabaeb252688a74558cca51768cb0ed3e0
Jerowe Tan <jerowe.tan99@gmail.com>
2023-04-05 13:58:47 +0800

    v0.4

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,33 +5,30 @@ import Icon from "@/utilities/Icon";
 export default () => {
     const [ creatorLoadmore, creatorLoadmoreSet ] = useState(false);
     return <>
-    <footer className="w-full flex justify-center px-2 pt-10 pb-8 bg-gray-900 text-slate-300">
+    <footer className="w-full flex justify-center sm:px-10 px-5 pt-10 pb-8 bg-gray-900 text-slate-300">
         <main className="w-[100rem] flex flex-wrap gap-10">
-            <section className="flex-1 flex gap-x-10">
-                <div className="flex-0">
+            <section className="flex-1 flex sm:flex-nowrap flex-wrap gap-x-10 mb-5">
+                <div className="flex-0 sm:w-auto w-full flex justify-center">
                     <div className="relative w-[10rem] aspect-square">
                         <Image src="/ONLYMECHANICS.svg" alt="Creator's Logo" priority fill className="w-full h-full object-contain object-center"/>
                     </div>
                 </div>
-                
-                <div className="flex-1">
-                    <h1 className="text-2xl font-semibold tracking-tighter pb-5">Creator</h1>
+                <div className="flex-1 sm:pt-0 pt-5">
+                    <h1 className="text-2xl font-semibold tracking-tighter pb-5 text-center">Creator</h1>
                     <p className="text-justify font-light">Learning a new instrument is quite a long journey. When I was learning how to play a guitar, along the way I needed to study music theory too. At first it is kind of easy as there are only{creatorLoadmore ?<> notes, tones, pitches and other terminologies. However, time comes that music becomes literally a mathematics, that's metaphorical but I like to call it that way because of how everything is related and relative to numbers like the binary system of computers. Then it got me thinking to find an app to help me learn the scale and fretboard and it turns out that most app are non-existent or an app needs a premium purchased or there is too much information to start with. So I kinda like maybe I make a new one that suits to my needs. Afterward, I created a web-application, Musifier (YES! I refuse to just use paper and pencil in learning Music Theory). It is actually not a bad thing for me, because I was also able to learn Next.JS and Laravel. It is quite tedious but It is a three birds in one bullet. </>:<>... </>} <a href="#" className="underline underline-offset-2 font-normal" onClick={()=>{creatorLoadmoreSet(prev=>!prev)}}>Load {creatorLoadmore?'less':'more'}</a></p>
                 </div>
             </section>
-            <section className="flex-0 w-3/12">
-                <div className="flex-1">
-                    <h1 className="text-2xl font-semibold tracking-tighter pb-5">Socials</h1>
-                    <div className="w-ful flex flex-wrap gap-5">
-                        <div className="w-8 h-8 p-1 rounded-lg bg-slate-300 cursor-pointer" onClick={()=>{window.open('https://www.fb.com/mekaniRxON', '_blank');}}>
-                            <Icon name="facebook" tailwindCalss="fill-gray-900" />
-                        </div>
-                        <div className="w-8 h-8 p-1 rounded-lg bg-slate-300 cursor-pointer" onClick={()=>{window.open('https://www.twitter.com/Mekani_Tekno', '_blank');}}>
-                            <Icon name="twitter" tailwindCalss="fill-gray-900" />
-                        </div>
-                        <div className="w-8 h-8 p-1 rounded-lg bg-slate-300 cursor-pointer flex items-center" onClick={()=>{window.open('https://www.youtube.com/@mekani_tekno', '_blank');}}>
-                            <Icon name="youtube" tailwindCalss="fill-gray-900" />
-                        </div>
+            <section className="flex-0 md:w-3/12 w-full">
+                <h1 className="text-2xl font-semibold tracking-tighter pb-5 md:text-start text-center">Socials</h1>
+                <div className="w-ful flex flex-wrap gap-5 md:justify-start justify-center">
+                    <div className="w-8 h-8 p-1 rounded-lg bg-slate-300 cursor-pointer" onClick={()=>{window.open('https://www.fb.com/mekaniRxON', '_blank');}}>
+                        <Icon name="facebook" tailwindClass="fill-gray-900" />
+                    </div>
+                    <div className="w-8 h-8 p-1 rounded-lg bg-slate-300 cursor-pointer" onClick={()=>{window.open('https://www.twitter.com/Mekani_Tekno', '_blank');}}>
+                        <Icon name="twitter" tailwindClass="fill-gray-900" />
+                    </div>
+                    <div className="w-8 h-8 p-1 rounded-lg bg-slate-300 cursor-pointer flex items-center" onClick={()=>{window.open('https://www.youtube.com/@mekani_tekno', '_blank');}}>
+                        <Icon name="youtube" tailwindClass="fill-gray-900" />
                     </div>
                 </div>
             </section>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,31 +1,85 @@
 import Link from "next/link";
-
+import { useState } from "react";
+/// css
+import myStyle from './Navbar.module.css';
 /// utilities
 import Icon from "@/utilities/Icon";
 
 export default () => {
+    // useState
+    const [ selectionItem, selectionItemSet ] = useState(false);
+
+
+    // populate - Navbar Selection
+    const selectionContent = {
+        instrument:<>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Electric Guitar</span>
+            </Link>
+            <div>
+                <span className="text-slate-500">...in Beta</span>
+            </div>
+        </>,
+        theory:<>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Terminologies</span>
+            </Link>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Intervals</span>
+            </Link>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Scales</span>
+            </Link>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Chords</span>
+            </Link>
+        </>,
+        join:<>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Login</span>
+            </Link>
+            <Link className={`${myStyle.select} cursor-pointer flex flex-row mb-2`} href={`/`}>
+                <div className={`${myStyle.light} w-1 h-[5px]"`}></div>
+                <span className="ml-1">Sign-up</span>
+            </Link>
+        </>,
+    }
+    
     return <>
     <header className="relative">
         {/*First Nav*/}
-        <nav className="fixed w-full h-20 gap-x-12 flex justify-center bg-gray-900 text-slate-200 after:absolute after:bottom-0 after:translate-y-full after:h-[0.2rem] after:w-full after:bg-gradient-to-r after:from-sky-400 after:to-blue-500 after:drop-shadow-lg">
+        <nav className="fixed z-10 w-full h-20 gap-x-12 flex justify-center bg-gray-900 text-slate-200 after:absolute after:bottom-0 after:translate-y-full after:h-[0.2rem] after:w-full after:bg-gradient-to-r after:from-sky-400 after:to-blue-500 after:drop-shadow-lg">
             {/*Logo Container*/}
             <Link className="cursor-pointer" href={'/'}>
                 <Icon name="musifier_dark" className="w-36 h-full"/>
             </Link>
             {/*HyperLinks*/}
-            <div className="gap-x-10 flex flex-row items-center text-xl font-semibold tracking-wide">
-                <div className="flex flex-row items-center cursor-pointer">
+            <div className="relative gap-x-10 flex flex-row items-center text-xl font-semibold tracking-wide">
+                <div className="flex flex-row items-center cursor-pointer" onClick={()=>{selectionItemSet(prev=>prev=='instrument'?false:'instrument')}}>
                     <span>Instruments</span>
-                    <Icon name="down" tailwindClass="fill-slate-300" className="h-8 ml-[-4px] w-full"/>
+                    <Icon name={selectionItem == 'instrument' ?`up`:`down`} tailwindClass="fill-slate-500" className="h-8 ml-[-4px] w-full"/>
                 </div>
-                <div className="flex flex-row items-center cursor-pointer">
+                <div className="flex flex-row items-center cursor-pointer" onClick={()=>{selectionItemSet(prev=>prev=='theory'?false:'theory')}}>
                     <span>Theory</span>
-                    <Icon name="down" tailwindClass="fill-slate-300" className="h-8 ml-[-4px] w-full"/>
+                    <Icon name={selectionItem == 'theory' ?`up`:`down`} tailwindClass="fill-slate-500" className="h-8 ml-[-4px] w-full"/>
                 </div>
-                <div className="flex flex-row items-center cursor-pointer">
+                <div className="flex flex-row items-center cursor-pointer" onClick={()=>{selectionItemSet(prev=>prev=='join'?false:'join')}}>
                     <span>Join</span>
-                    <Icon name="down" tailwindClass="fill-slate-300" className="h-8 ml-[-4px] w-full"/>
+                    <Icon name={selectionItem == 'join' ?`up`:`down`} tailwindClass="fill-slate-500" className="h-8 ml-[-4px] w-full"/>
                 </div>
+                {
+                    selectionItem ?
+                    <div className={`absolute bottom-0 translate-y-full z-10 w-auto h-auto rounded-b-xl flex flex-col shadow-md bg-gray-900 font-normal tracking-tight text-lg p-5 ${selectionItem ? myStyle.SlideDown : ''}`}>
+                        {selectionContent[selectionItem]}
+                    </div> : <></>
+                }
+                
             </div>
         </nav>
 
